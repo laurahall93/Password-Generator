94f1e78f8c4526a5096f4c03c5f85c0314b27030
Laura Hall <113890455+laurahall93@users.noreply.github.com>
2023-01-23 15:59:34 +0000

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+// "/"];
+
+let generatePassword1El = document.getElementById("generate-password1")
+let generatePassword2El = document.getElementById("generate-password2")
+
+function randomPasword() {
+    const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
+    "/"];
+    var passwordLength = 16;
+    var password = "";
+    for (var i = 0; i <= passwordLength; i++) {
+      var randomNumber = Math.floor(Math.random() * characters.length);
+      password += characters[randomNumber];
+    }
+    return password 
+
+}
+
+function getRandomPassword() {
+    generatePassword1El.textContent = randomPasword();
+    generatePassword2El.textContent = randomPasword();
+}
+
